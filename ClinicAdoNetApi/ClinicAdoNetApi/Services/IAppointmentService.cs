0c16394649c68dda82a7ce160c901c4f56dd67f1
s33045 <s33045@pjwstk.edu.pl>
2026-04-28 17:23:40 +0200

    feat(Services): add GetAllAsync method to IAppointmentService

--- ClinicAdoNetApi/ClinicAdoNetApi/Services/IAppointmentService.cs
+++ ClinicAdoNetApi/ClinicAdoNetApi/Services/IAppointmentService.cs
@@ -1,5 +1,8 @@
-﻿namespace ClinicAdoNetApi.Services;
+﻿using ClinicAdoNetApi.DTOs;
+
+namespace ClinicAdoNetApi.Services;
 
 public interface IAppointmentService
 {
+    Task<List<AppointmentListDto>> GetAllAsync(string? status, string? patientLastName, int? idDoctor);
 }
